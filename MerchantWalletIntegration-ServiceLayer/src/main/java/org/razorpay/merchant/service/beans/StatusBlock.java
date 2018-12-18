f7d44359376ae43fd1cf64ebd66ac82818510cf0
parmar <parmar@DESKTOP-4Q80583>
2018-12-18 16:26:42 +0530

    response code

--- MerchantWalletIntegration-ServiceLayer/src/main/java/org/razorpay/merchant/service/beans/StatusBlock.java
+++ MerchantWalletIntegration-ServiceLayer/src/main/java/org/razorpay/merchant/service/beans/StatusBlock.java
@@ -3,4 +3,29 @@ package org.razorpay.merchant.service.beans;
 public class StatusBlock {
 private String responseMessage;
 private String status;
+public String getResponseMessage() {
+	return responseMessage;
+}
+public void setResponseMessage(String responseMessage) {
+	this.responseMessage = responseMessage;
+}
+public String getStatus() {
+	return status;
+}
+public void setStatus(String status) {
+	this.status = status;
+}
+@Override
+public String toString() {
+	StringBuilder builder = new StringBuilder();
+	builder.append("StatusBlock [responseMessage=");
+	builder.append(responseMessage);
+	builder.append(", status=");
+	builder.append(status);
+	builder.append("]");
+	return builder.toString();
+}
+ 
+
+
 }
